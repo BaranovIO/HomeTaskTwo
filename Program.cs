aa6ab8fbeb06a14d87cfeceafd2f6ed12b681642
Igor Baranov <eltery@mail.ru>
2022-12-02 22:30:29 +0300

    the second digit task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int CutNumber (int number)
+{
+    int secondDigit = (number / 10 ) % 10;
+    return secondDigit;
+}
+
+Console.Write("Введите трёхначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = CutNumber(userNumber);
+Console.WriteLine($"Вторая цифра вашего числа: {result}");
